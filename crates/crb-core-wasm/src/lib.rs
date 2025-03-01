7cd8f919acfad8c0d92d9655c57e6cccb7534b63
Denis Kolodin <deniskolodin@gmail.com>
2025-03-01 11:55:27 +0100

    Change wasm feature to web

--- crates/crb-core-wasm/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub use web_time::Instant;
-
-#[cfg(all(
-    target_arch = "wasm32",
-    target_vendor = "unknown",
-    target_os = "unknown"
-))]
-pub use tokio_with_wasm::{spawn, spawn_local, task::JoinHandle};
--- examples/web-app/src/main.rs
+++ examples/web-app/src/main.rs
@@ -1,13 +1,11 @@
-use crb::agent::Runnable;
+use crb::agent::Standalone;
 use web_app::Frontend;
-use tokio_with_wasm::alias as tokio;
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() {
+fn main() {
     console_error_panic_hook::set_once();
     // TODO: Is that possible to use `tracing` package here?
     let config = wasm_logger::Config::new(log::Level::Info);
     wasm_logger::init(config);
     log::info!("WEB APP LOADED");
-    Frontend::new().run().await;
+    Frontend::new().spawn();
 }
