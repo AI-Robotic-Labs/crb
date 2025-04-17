32c3d347a659a0a7caf53d0824b4a42d851faf8c
Denis Kolodin <deniskolodin@gmail.com>
2025-04-17 07:23:37 +0200

    Support any deserializable type

--- blocks/crb-file/src/config.rs
+++ blocks/crb-file/src/config.rs
@@ -5,6 +5,16 @@ use async_trait::async_trait;
 use std::path::PathBuf;
 use toml::Value;
 use tokio::fs;
+use std::marker::PhantomData;
+use serde::de::DeserializeOwned;
+
+pub trait Config: DeserializeOwned + Send + 'static {
+    // TODO: const NAMESPACE
+}
+
+impl<T> Config for T
+where T: DeserializeOwned + Send + 'static {}
+
 
 pub struct ConfigAgent {
 }
@@ -13,18 +23,20 @@ impl Agent for ConfigAgent {
     type Context = AgentSession<Self>;
 }
 
-pub struct ReadConfig {
+pub struct ReadConfig<T = Value> {
     pub path: PathBuf,
+    pub _type: PhantomData<T>,
 }
 
-impl Request for ReadConfig {
-    type Response = Value;
+impl<T: Config> Request for ReadConfig<T> {
+    type Response = T;
 }
 
 #[async_trait]
-impl OnRequest<ReadConfig> for ConfigAgent {
-    async fn on_request(&mut self, msg: ReadConfig, _ctx: &mut Context<Self>) -> Result<Value> {
+impl<T: Config> OnRequest<ReadConfig<T>> for ConfigAgent {
+    async fn on_request(&mut self, msg: ReadConfig<T>, _ctx: &mut Context<Self>) -> Result<T> {
         let content = fs::read_to_string(&msg.path).await?;
+        // TODO: Detect file type, override values from a namespace
         let config = toml::from_str(&content)?;
         Ok(config)
     }
